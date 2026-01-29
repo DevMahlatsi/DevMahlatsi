c49bffb141439e9f0a108f3d631812a036cfb3f4
DevMahlatsi <j.mahlatsi@outlook.com>
2026-01-29 19:05:46 +0200

    last update of the day.

--- src/Components/Design Process/DesignProcess.tsx
+++ src/Components/Design Process/DesignProcess.tsx
@@ -3,13 +3,13 @@ import { designProcess } from "../../Data/devmahlatsi.data";
 export default function ProcessDesign(){
   return(
     <>
-      <div className="flex gap-2">
+      <div className="flex gap-8 flex-wrap justify-center ">
         {designProcess.map((process) => {
           return(
             <>
-            <div className="border ">
-              <div className="bg-red-700 inline-block">
-                <h1 className="p-3">
+            <div className="border border-[#1a73e8] rounded-2xl p-4 w-65">
+              <div className="bg-[#1a73e8] inline-block rounded-2xl">
+                <h1 className="text-xl p-2 px-4 ">
                   {process.id}
                 </h1>
               </div>
