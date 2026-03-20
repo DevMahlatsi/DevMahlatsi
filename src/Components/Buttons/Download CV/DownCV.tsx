c5cf3e02eeed583aa41aa3500c066a9061a782b2
DevMahlatsi <j.mahlatsi@outlook.com>
2026-03-20 23:45:55 +0200

    CV button now works very well.

--- src/Components/Buttons/Download CV/DownCV.tsx
+++ src/Components/Buttons/Download CV/DownCV.tsx
@@ -1,11 +1,12 @@
 import downloadCV from "../../../assets/downloadCV.svg";
+import LugisaniCV from "../../../assets/Lugisani Jeremiah Mahlatsi CV.pdf"
 
 export default function DownCV(){
   return(
     <>
       <button className="hero-buttons hover:bg-[rgba(19,19,19,0.62)] bg-[rgba(63,63,63,0.62)] border-2 border-[#1a73e8]">
         <img className="h-7" src={downloadCV} alt="download icon" />
-        <a download={true} href={downloadCV}>Download CV</a>
+        <a target="_blank" download={true} href={LugisaniCV}>Download CV</a>
       </button>
     </>
   )
