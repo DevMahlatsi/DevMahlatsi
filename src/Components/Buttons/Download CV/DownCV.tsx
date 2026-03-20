2521bbe553d4ca1f162c311a8fef800fe716ab32
DevMahlatsi <j.mahlatsi@outlook.com>
2026-03-20 23:33:51 +0200

    Hover fix for download CV

--- src/Components/Buttons/Download CV/DownCV.tsx
+++ src/Components/Buttons/Download CV/DownCV.tsx
@@ -3,9 +3,9 @@ import downloadCV from "../../../assets/downloadCV.svg";
 export default function DownCV(){
   return(
     <>
-      <button className="hero-buttons bg-[rgba(63,63,63,0.62)] border-2 border-[#1a73e8]">
+      <button className="hero-buttons hover:bg-[rgba(19,19,19,0.62)] bg-[rgba(63,63,63,0.62)] border-2 border-[#1a73e8]">
         <img className="h-7" src={downloadCV} alt="download icon" />
-        Download CV
+        <a download={true} href={downloadCV}>Download CV</a>
       </button>
     </>
   )
