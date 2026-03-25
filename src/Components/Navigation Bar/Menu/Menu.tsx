0a83280782dd7b450f02310741e2f9a6d537202a
DevMahlatsi <j.mahlatsi@outlook.com>
2026-03-25 23:07:29 +0200

    navigation fixed. Interation will be added. Need to learn how to dynamically slide down the website without changing my url.

--- src/Components/Navigation Bar/Menu/Menu.tsx
+++ src/Components/Navigation Bar/Menu/Menu.tsx
@@ -2,19 +2,17 @@ import { menuData } from "../../../Data/devmahlatsi.data";
 
 export default function Menu(){
   return(
-    <>
       <div className="hidden sm:block">
-        <ul className="text-sm md:text-xl flex md:gap-16 gap-5">
+        <ul className="flex items-center text-sm md:text-xl  md:gap-16 gap-5">
           {menuData.map((menu, index) => {
             return(
               <>
-                <li key={index} className="hover:cursor-pointer hover:" >{menu.title.toUpperCase()}</li>
+                <li key={index} className="cursor-pointer font-medium text-white transition-all duration-300 
+                       hover:scale-105 hover:text-[#1a73e8] hover:drop-shadow-[0_0_8px_rgba(26,115,232,0.8)]" >{menu.title.toUpperCase()}</li>
               </>
             )
           })}
         </ul>
       </div>
-      
-    </>
   )
 }
