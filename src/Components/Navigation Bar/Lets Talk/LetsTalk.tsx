db7ff3d5c706a7a516eed94a869df1dd001a5b63
DevMahlatsi <j.mahlatsi@outlook.com>
2026-03-25 23:19:14 +0200

    Lets talk button is as interactive only on desktop.

--- src/Components/Navigation Bar/Lets Talk/LetsTalk.tsx
+++ src/Components/Navigation Bar/Lets Talk/LetsTalk.tsx
@@ -9,7 +9,9 @@ export default function Talk(){
                    transition-all duration-300 ease-in-out
                    hover:bg-[#1e88e5] hover:scale-105 hover:shadow-[0_0_20px_rgba(26,115,232,0.6)]
                    active:scale-95">
-          <h1 className=' border-none text-white border rounded-4xl p-3 bg-[#1a73e8]'>Lets Talk</h1>
+          <span>Lets talk</span>
+          <HiArrowRight
+          className="text-lg transition-transform duration-300 group-hover:translate-x-1"/>
         </button>
       </div>
     </>
